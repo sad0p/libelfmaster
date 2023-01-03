ccba3dbfbc69b21198ae8aeeeffb5931d1f3c2e5
sad0p <sad0p@protonmail.com>
2023-01-03 18:25:56 -0500

    acknowledge false return by not writing to struct in ElfDynSymTabCount()

--- bindings/go/libelfmaster/libelfmaster.go
+++ bindings/go/libelfmaster/libelfmaster.go
@@ -244,7 +244,10 @@ func (o *ElfObj) ElfSymTabCount(count *uint64) (ret bool) {
 func (o *ElfObj) ElfDynSymTabCount(count *uint64) (ret bool) {
 	var localCount C.uint64_t
 	ret = intToBool(int(C.elf_dynsym_count_w(&o.obj, &localCount)))
-	*count = uint64(localCount)
+
+	if ret {
+		*count = uint64(localCount)
+	}
 	return
 }
 
